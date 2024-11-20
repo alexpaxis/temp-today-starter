723c8263fce2bd206c9387cb90043a99bf86a078
Alexandros_Paximadakis <alexandros.paximadakis@abl.de>
2024-11-20 09:46:40 +0100

    Exercise 4: Refactor WeatherDisplay to use styled-components

--- src/components/WeatherDisplay.tsx
+++ src/components/WeatherDisplay.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
-
+import styled from "styled-components/native";
 
 type WeatherProps = 
 {
@@ -7,31 +7,50 @@ type WeatherProps =
     condition: string;
 }
 
+const StyledContainer = styled.View`
+  align-items: center;
+  justify-content: center;
+  width: "100%";
+`;
+
+const StyledTextTemperature = styled.Text`
+  padding-top: 40;
+  color: "#333";
+  font-size: 48px;
+  font-weight: bold;
+`;
+
+const StyledTextCondition = styled.Text`
+  font-size: 24px;
+  color: "#666";
+  margin-top: 10px;
+`;
+
 const WeatherDisplay: React.FC<WeatherProps> = ({temperature,condition}) => 
-    {
-        return  <View style={styles.container}>
-                    <Text style={styles.temperature}>{temperature}°C</Text>
-                    <Text style={styles.condition}>{condition}</Text>
-                </View>
-    }
+{
+    return  <StyledContainer>
+                <StyledTextTemperature>{temperature}°C</StyledTextTemperature>
+                <StyledTextCondition>{condition}</StyledTextCondition>
+            </StyledContainer>
+}
 
-    const styles = StyleSheet.create({
-        container: {
-          alignItems: "center",
-          justifyContent: "center",
-          width: "100%"
-        },
-          temperature: {
-            paddingTop: 40,
-            color: "#333",
-            fontSize: 48,
-            fontWeight: "bold",
-          },
-          condition: {
-            marginTop: 10,
-            color: "#666",
-            fontSize: 24
-          }
-      });
+    // const styles = StyleSheet.create({
+    //     container: {
+    //       alignItems: "center",
+    //       justifyContent: "center",
+    //       width: "100%"
+    //     },
+    //       temperature: {
+    //         paddingTop: 40,
+    //         color: "#333",
+    //         fontSize: 48,
+    //         fontWeight: "bold",
+    //       },
+    //       condition: {
+    //         marginTop: 10,
+    //         color: "#666",
+    //         fontSize: 24
+    //       }
+    //   });
 
-    export default WeatherDisplay;
+export default WeatherDisplay;
